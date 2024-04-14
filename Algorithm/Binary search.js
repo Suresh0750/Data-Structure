ac9cf0cfbf88682e19f8a5e2f975ce4f6a6de692
Suresh0750 <sureshvarma0750@gmail.com>
2024-04-14 03:22:45 -0700

    binarySearch

--- Algorithm/Binary search.js
+++ Algorithm/Binary search.js
@@ -1,5 +1,5 @@
 
-
+// * normal logic I put this logic
 function binary(arr,key){
 
     let left=0
@@ -29,20 +29,51 @@ function binary(arr,key){
 // console.log(binary([1,2,3,4,5,6],7))
 
 
-function binarySearch(arr,target){
+// function binarySearch(arr,target){
 
-    let left = 0
-    let right = arr.length-1
+//     let left = 0
+//     let right = arr.length-1
     
 
-    while(left<=right){
+//     while(left<=right){
 
-        let mid = Math.floor((right+left)/2)
+//         let mid = Math.floor((right+left)/2)
 
-        if(target == arr[mid]) return mid
+//         if(target == arr[mid]) return mid
 
-        if(target <arr[mid]) right = mid-1
-        else left = mid+1
+//         if(target <arr[mid]) right = mid-1
+//         else left = mid+1
+//     }
+
+// }
+
+
+function binarySearch(arr,target){
+
+    let start = 0
+    let end = arr.length
+
+    while(start<=end){
+
+        let mid =Math.floor((start+end)/2)
+
+        if(arr[mid] == target){
+
+            return mid
+        }else if(arr[mid]<target){
+
+            start = mid+1
+
+        }else {
+            end = end-1
+        }
     }
-
+    
+    return -1
 }
+
+
+
+console.log(binarySearch([1,2,3,4,5],3))
+console.log(binarySearch([2,4,6,8,10],8))
+console.log(binarySearch([3,6,9,12],6))
